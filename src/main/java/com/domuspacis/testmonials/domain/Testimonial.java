0cc87178f45781d93feb066e2a41122975aeefb4
Ananie2024 <anahafasha@gmail.com>
2026-08-05 12:22:49 +0200

    m

--- src/main/java/com/domuspacis/testmonials/domain/Testimonial.java
+++ src/main/java/com/domuspacis/testmonials/domain/Testimonial.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 @SuperBuilder
 @Table(name = "testimonials")
